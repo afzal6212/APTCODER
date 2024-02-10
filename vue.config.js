9e846d1b4f152ca6f770132f1903fb1f07dfdf54
afzal6212 <107555085+afzal6212@users.noreply.github.com>
2024-02-10 21:27:00 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	// The base URL your application bundle will be deployed at
 	"publicPath": process.env.NODE_ENV === "production" ? "/vue-todolist/" : "/",
 
-	// Add configuration for use Dart sass/scss and compile files of 'assets' folder
+	// Add configuration for use Dart sass/scss and compile files of 'assets'folder
 	"pluginOptions": {
 		"style-resources-loader": {
 			"preProcessor": "scss",
